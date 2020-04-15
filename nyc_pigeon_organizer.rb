5ceebf2220d7e14e6faa9c5a92f8acae9b254478
Jesse Langley <jlangleyiii@aol.com>
2020-04-15 14:27:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,47 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+a = {}
+#names_array = []
+sorted_member_list = []
+#info = []
+pigeon_list = []
+#count = 0
+#count2 = 1
+          data.reduce({}) do |memo, (key, value)|
+
+ key
+value
+        value.reduce({}) do |memo, (key2, value2)|
+
+info = key2.to_s
+names_array = value2
+     names_array
+
+ names_array.each do |name| 
+        
+        pigeon_list << {name => { key => [info]}}
+      
+
+
+memo
 end
+memo
+end
+end
+  
+ pigeon_list.each_index do |index|
+              unless index == 0
+              
+ a = pigeon_list[0].merge!(pigeon_list[index]){|key,val1,val2|val1.merge(val2){|k,v1,v2|v1+v2}}
+   
+
+
+ 
+  end     
+end
+
+
+a
+
+end
+
